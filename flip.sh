240852932d15ae611a681eac22804dde204f9ab2
sanket2498 <sanketdulange24@gmail.com>
2020-11-04 21:45:19 +0530

    Added : Multiple Flips

--- flip.sh
+++ flip.sh
@@ -1,8 +1,15 @@
 echo "Flip Coin Simulator""
-randomValue=$(($RANDOM%2+1));
-if [ $randomValue -eq 1 ]
-then
-	echo "Head"   
-else
-	echo "Tail"
-fi
+head=0
+tail=0
+while [[ $head -lt 10 && $tail -lt 10 ]]
+do
+	randomValue=$(($RANDOM%2+1));
+	if [ $randomValue -eq 1 ]
+	then
+        	((head++))
+	else
+        	((tail++))
+	fi
+done
+echo "Head won $head times"
+echo "Tail won $tail times"
