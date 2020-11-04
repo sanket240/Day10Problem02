0fc7e43347deef44cd7dfbc3537710a9b7bfd620
sanket2498 <sanketdulange24@gmail.com>
2020-11-04 21:49:26 +0530

    Modified : Use Case 2

--- flip.sh
+++ flip.sh
@@ -1,7 +1,7 @@
 echo "Flip Coin Simulator""
 head=0
 tail=0
-while [[ $head -lt 10 && $tail -lt 10 ]]
+while [[ $head -lt 21 && $tail -lt 21 ]]
 do
 	randomValue=$(($RANDOM%2+1));
 	if [ $randomValue -eq 1 ]
@@ -11,5 +11,17 @@ do
         	((tail++))
 	fi
 done
-echo "Head won $head times"
-echo "Tail won $tail times"
+if [ $head -eq $tail ]
+then
+	echo "Tie"
+elif [ $head -gt $tail ]
+then
+	echo "Head won"
+	a=$(($head-$tail))
+	echo "Head won by $a flips"
+else
+	echo "Tail Won"
+	b=$(($tail-$head))
+	echo "Tail won by $b flips"
+fi
+
